a156feb8cdf867b993c5597a71a41da01a0eee8d
Hack Reactor Student <student@hackreactor.com>
2017-09-18 18:43:08 -0700

    tables have been created correctly

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,70 @@
-CREATE DATABASE chat;
+
+
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
-/* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Messages`;
+    
+CREATE TABLE `Messages` (
+  `id` INTEGER AUTO_INCREMENT,
+  `body` VARCHAR(150),
+  `user` INTEGER,
+  `postdate` DATETIME,
+  `room` VARCHAR(20),
+  PRIMARY KEY (`id`)
+  -- FOREIGN KEY (`user`) REFERENCES `Users` (`id`)
+);
+
+-- ---
+-- Table 'Users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Users`;
+    
+CREATE TABLE `Users` (
+  `id` INTEGER AUTO_INCREMENT,
+  `name` VARCHAR(30),
+  `age` INTEGER,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `Messages` ADD FOREIGN KEY (user) REFERENCES `Users` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Messages` (`id`,`body`,`user`,`postdate`,`room`) VALUES
+-- ('','','','','');
+-- INSERT INTO `Users` (`id`,`name`,`age`) VALUES
+-- ('','','');
